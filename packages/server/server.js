393e0b9ba128ae1b351ffc34a3e7bd387de3fd53
carlos <carlos.limalopes@outlook.com>
2023-04-27 17:36:36 -0300

    Deploy prep

--- packages/server/server.js
+++ packages/server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config({ path: './config/.env' });
 const connectBD = require('./config/db');
+const { errorHandler } = require('./middleware/errorHandler');
 const userRoutes = require('./routes/user');
 const transactionRoutes = require('./routes/transaction');
 const PORT = process.env.PORT || 5000;
@@ -17,6 +18,17 @@ app.use(express.json());
 // Routes
 app.use('/user', userRoutes);
 app.use('/transaction', transactionRoutes);
+
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+  });
+}
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server Started on Port ${PORT}`);
