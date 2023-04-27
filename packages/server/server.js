e366be516c098697a31835915a7ccf0be4f7bc0c
carlos <carlos.limalopes@outlook.com>
2023-04-27 18:32:15 -0300

    Initial Commit

--- packages/server/server.js
+++ packages/server/server.js
@@ -22,6 +22,11 @@ app.use('/transaction', transactionRoutes);
 //Serve Frontend
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
 }
 
 app.use(errorHandler);
